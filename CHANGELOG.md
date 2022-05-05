a0e5ceb14ad720a295fd77d86aa93f555f68414f
justin <justin@yosmart.com>
2022-05-05 23:40:21 +0800

    update package

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-## 0.0.3 (2022.5.2)
-
-* add yolink client support
--- setup.py
+++ setup.py
@@ -3,26 +3,25 @@ from setuptools import setup
 
 setup(
     name="yolink-api",
-    version="0.0.3",
+    version="0.0.4",
     author="YoSmart",
     description="A library to authenticate with yolink device",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/YoSmart-Inc/yolink-api.git"
-    license="GPL",
+    url="https://github.com/YoSmart-Inc/yolink-api",
+    project_urls={
+        "Bug Tracker": "https://github.com/YoSmart-Inc/yolink-api/issues",
+    },
+    license="MIT",
     keywords="yolink api",
     packages=["yolink"],
     zip_safe=False,
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires=">=3.6",
     install_requires=[
         "aiohttp",
         "paho-mqtt==1.6.1",
