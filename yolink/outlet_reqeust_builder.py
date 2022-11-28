0f195888b910e8638faab71e37bec5feb335a06a
matrix <solitudek2@gmail.com>
2022-11-28 23:01:00 +0800

    maker api simpler

--- yolink/outlet_reqeust_builder.py
+++ yolink/outlet_reqeust_builder.py
@@ -1,30 +1,16 @@
 """Outlet reqeust builder"""
+from __future__ import annotations
 
 from .client_reqeust import ClientReqeust
-from .reqeust_builder import RequestBuilder
 
 
-class OutletReqeustBuilder(RequestBuilder):
+class OutletReqeustBuilder:
     """Outlet request builder"""
 
-    def __init__(self) -> None:
-        super().__init__()
-        self._state: str = None
-        self._plug_index: str | None = None
-
-    def state(self, state: str):
-        """Set state"""
-        self._state = state
-        return self
-
-    def plug_index(self, plug_index: int):
-        """Set plug index"""
-        self._plug_index = plug_index
-        return self
-
-    def build(self) -> ClientReqeust:
-        """Create outlet reqeust params"""
-        params: dict[str, str | int] = {"state": self._state}
-        if self._plug_index is not None:
-            params["chs"] = 1 << self._plug_index
-        return ClientReqeust(self._method, params)
+    @classmethod
+    def set_state_reqeust(cls, state: str, plug_indx: int | None) -> ClientReqeust:
+        """Set device state."""
+        params: dict[str, str | int] = {"state": state}
+        if plug_indx is not None:
+            params["chs"] = 1 << plug_indx
+        return ClientReqeust("setState", params)
